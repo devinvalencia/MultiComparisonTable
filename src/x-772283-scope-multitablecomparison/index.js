82fec99aea52a6aec9b20ced14499f9c3b7b1b81
Devin Valencia <DevinValencia@yahoo.com>
2022-04-25 19:08:29 -0400

    Creating Object based off fields selected

--- src/x-772283-scope-multitablecomparison/index.js
+++ src/x-772283-scope-multitablecomparison/index.js
@@ -82,13 +82,92 @@ createCustomElement("x-772283-scope-multitablecomparison", {
 	view,
 	styles,
 	actionHandlers: {
-		TEST_CLICK_ACTION: ({ action, properties }) => {
+		TEST_CLICK_ACTION: ({ action, properties, dispatch }) => {
 			// How to access payload, desctructure property from payload
-			const { value } = action.payload;
+			const { eventData } = action.payload;
 
 			// Update Properties
 			// properties.testValues.push(event.path[0].innerText);
-			console.log(value.childNodes);
+
+			var obj = {};
+
+			for (var node in eventData.path[2].childNodes) {
+				if (
+					node < eventData.path[2].childNodes.length &&
+					eventData.path[0].className != "fieldLabel" &&
+					eventData.path[2].childNodes[node].nodeName != "TR"
+				) {
+					// If node in row matches text of currently selected cell
+					if (
+						eventData.path[2].childNodes[node].innerText ==
+						eventData.path[1].innerText
+					) {
+						// If node already selected, make unselected
+						if (eventData.path[2].childNodes[node].className == "selected") {
+							if (eventData.path[2].childNodes[node].nodeName != "TH") {
+								eventData.path[2].childNodes[node].className =
+									"notSelectedField";
+							} else if (
+								eventData.path[2].childNodes[node].className != "ignore"
+							) {
+								eventData.path[2].childNodes[node].className =
+									"notSelectedRecord";
+							}
+						} else {
+							// Select all nodes that match innner text of current node to selected
+							eventData.path[2].childNodes[node].className = "selected";
+
+							// Update object
+							// var fieldNameLabel = eventData.path[2].childNodes[0].innerText;
+							// obj[fieldNameLabel] = eventData.path[0].innerText;
+							// console.log(obj);
+							// console.log(fieldNameLabel);
+						}
+					} else if (
+						eventData.path[2].childNodes[node].className != "fieldLabel"
+					) {
+						if (eventData.path[2].childNodes[node].nodeName != "TH") {
+							eventData.path[2].childNodes[node].className = "notSelectedField";
+						} else if (
+							eventData.path[2].childNodes[node].className != "ignore"
+						) {
+							eventData.path[2].childNodes[node].className =
+								"notSelectedRecord";
+						}
+					}
+				}
+			}
+
+			// console.log(eventData.path);
+
+			// Look at each row
+			for (var outerNode in eventData.path[3].childNodes) {
+				
+				// Look at each cell in row
+				for (var innerNode in eventData.path[3].childNodes[outerNode].childNodes) {
+
+					// IF classname on this cell is selected
+					// ADD property & value to obj
+					// console.log(eventData.path[3].childNodes[outerNode].childNodes[innerNode]);
+
+					if (eventData.path[3].childNodes[outerNode].childNodes[innerNode].className == 'selected') {
+						// console.log(`${eventData.path[3].childNodes[outerNode].childNodes[0].innerText} : ${eventData.path[3].childNodes[outerNode].childNodes[innerNode].innerText}`);
+						obj[eventData.path[3].childNodes[outerNode].childNodes[0].innerText] = eventData.path[3].childNodes[outerNode].childNodes[innerNode].innerText;
+						break;
+					}
+				}
+			}
+
+			// Print out final object
+			// dispatch('RUN_FINAL_TEST',{finalList: obj});
+
+			
+				console.log(obj);
+			
+		},
+		RUN_FINAL_TEST: ({ action }) => {
+			const { finalList } = action.payload;
+			console.log(finalList);
 		},
 	},
 	eventHandlers: [
@@ -102,49 +181,9 @@ createCustomElement("x-772283-scope-multitablecomparison", {
 					},
 				} = coeffects;
 
-				// console.log(event.path);
-
-				//var n = 0;
-				dispatch("TEST_CLICK_ACTION",{
-					value: event.path
+				dispatch("TEST_CLICK_ACTION", {
+					eventData: event,
 				});
-
-				for (var node in event.path[2].childNodes) {
-					if (
-						node < event.path[2].childNodes.length &&
-						event.path[0].className != "fieldLabel" &&
-						event.path[2].childNodes[node].nodeName != "TR"
-					) {
-						// If node in row matches text of currently selected cell
-						if (
-							event.path[2].childNodes[node].innerText ==
-							event.path[1].innerText
-						) {
-							// If node already selected, make unselected
-							if (event.path[2].childNodes[node].className == "selected") {
-								if (event.path[2].childNodes[node].nodeName != "TH") {
-									event.path[2].childNodes[node].className = "notSelectedField";
-								} else if (
-									event.path[2].childNodes[node].className != "ignore"
-								) {
-									event.path[2].childNodes[node].className =
-										"notSelectedRecord";
-								}
-							} else {
-								// Select all nodes that match innner text of current node to selected
-								event.path[2].childNodes[node].className = "selected";
-							}
-						} else if (
-							event.path[2].childNodes[node].className != "fieldLabel"
-						) {
-							if (event.path[2].childNodes[node].nodeName != "TH") {
-								event.path[2].childNodes[node].className = "notSelectedField";
-							} else if (event.path[2].childNodes[node].className != "ignore") {
-								event.path[2].childNodes[node].className = "notSelectedRecord";
-							}
-						}
-					}
-				}
 			},
 		},
 	],
