37aeb21dfb90d6cb2e7ac8eaefc7ef8ba5abf398
Devin Valencia <DevinValencia@yahoo.com>
2022-04-25 10:42:07 -0400

    Remembered Im bad at css

--- src/x-772283-scope-multitablecomparison/index.js
+++ src/x-772283-scope-multitablecomparison/index.js
@@ -1,88 +1,72 @@
 import { createCustomElement, actionTypes } from "@servicenow/ui-core";
 import snabbdom from "@servicenow/ui-renderer-snabbdom";
 import styles from "./styles.scss";
-import "./index.css";
 
 const view = (state, { updateState, dispatch }) => {
-	const styles = {
-		tableLabel: {
-			backgroundColor: "#d3d3d3",
-			fontWeight: "bold",
-		},
-	};
-
 	return (
 		<div>
 			<table>
 				{/* Always make first row */}
 				<tr>
 					{/* For Each record object.number in data resource array, make a header */}
-					<th class="ignore" style={{ border: "none" }}></th>
-					<th class="recordLabel" style={styles.tableLabel}>
+					<th className="ignore" style={{ border: "none" }}></th>
+					<th className="recordLabel">
 						<div>Record 1</div>
 					</th>
-					<th class="recordLabel" style={styles.tableLabel}>
+					<th className="recordLabel">
 						<div>Record 2</div>
 					</th>
-					<th class="recordLabel" style={styles.tableLabel}>
+					<th className="recordLabel">
 						<div>Record 3</div>
 					</th>
 				</tr>
 
 				{/* For Each field AND for each record obj in arr, find same field.value for each td (besides first) */}
 				<tr>
-					<td class="fieldLabel" style={styles.tableLabel}>
-						Field_1
-					</td>
-					<td class="fieldValue">
+					<td className="fieldLabel">Field_1</td>
+					<td className="fieldValue">
 						<div>FieldValue1</div>
 					</td>
-					<td class="fieldValue">
+					<td className="fieldValue">
 						<div>FieldValue1</div>
 					</td>
-					<td class="fieldValue">
+					<td className="fieldValue">
 						<div>FieldValue3</div>
 					</td>
 				</tr>
 				<tr>
-					<td class="fieldLabel" style={styles.tableLabel}>
-						Field_2
-					</td>
-					<td class="fieldValue">
+					<td className="fieldLabel">Field_2</td>
+					<td className="fieldValue">
 						<div>FieldValue1</div>
 					</td>
-					<td class="fieldValue">
+					<td className="fieldValue">
 						<div>FieldValue2</div>
 					</td>
-					<td class="fieldValue">
+					<td className="fieldValue">
 						<div>FieldValue1</div>
 					</td>
 				</tr>
 				<tr>
-					<td class="fieldLabel" style={styles.tableLabel}>
-						Field_3
-					</td>
-					<td class="fieldValue">
+					<td className="fieldLabel">Field_3</td>
+					<td className="fieldValue">
 						<div>FieldValue1</div>
 					</td>
-					<td class="fieldValue">
+					<td className="fieldValue">
 						<div>FieldValue2</div>
 					</td>
-					<td class="fieldValue">
+					<td className="fieldValue">
 						<div>FieldValue2</div>
 					</td>
 				</tr>
 				<tr>
-					<td class="fieldLabel" style={styles.tableLabel}>
-						Field_4
-					</td>
-					<td class="fieldValue">
+					<td className="fieldLabel">Field_4</td>
+					<td className="fieldValue">
 						<div>FieldValue1</div>
 					</td>
-					<td class="fieldValue">
+					<td className="fieldValue">
 						<div>FieldValue1</div>
 					</td>
-					<td class="fieldValue">
+					<td className="fieldValue">
 						<div>FieldValue1</div>
 					</td>
 				</tr>
@@ -117,20 +101,41 @@ createCustomElement("x-772283-scope-multitablecomparison", {
 
 				// path[0] = div in cell, path[1] = cell, path[2] = row
 				// For each cell in table row
+
+				var n = 0;
+
 				for (var node in event.path[2].childNodes) {
-					if (node < event.path[2].childNodes.length) {
+					if (
+						node < event.path[2].childNodes.length &&
+						event.path[0].className != "fieldLabel"
+					) {
 						// If node in row matches text of currently selected cell
 						if (
 							event.path[2].childNodes[node].innerText ==
 							event.path[1].innerText
 						) {
+							// If node already selected, make unselected
 							if (event.path[2].childNodes[node].className == "selected") {
-								event.path[2].childNodes[node].className = "notSelectedField";
+								if (event.path[2].childNodes[node].nodeName != "TH") {
+									event.path[2].childNodes[node].className = "notSelectedField";
+								} else if (
+									event.path[2].childNodes[node].className != "ignore"
+								) {
+									event.path[2].childNodes[node].className =
+										"notSelectedRecord";
+								}
 							} else {
+								// Select all nodes that match innner text of current node to selected
 								event.path[2].childNodes[node].className = "selected";
 							}
-						} else {
-							event.path[2].childNodes[node].className = "notSelectedField";
+						} else if (
+							event.path[2].childNodes[node].className != "fieldLabel"
+						) {
+							if (event.path[2].childNodes[node].nodeName != "TH") {
+								event.path[2].childNodes[node].className = "notSelectedField";
+							} else if (event.path[2].childNodes[node].className != "ignore") {
+								event.path[2].childNodes[node].className = "notSelectedRecord";
+							}
 						}
 					}
 				}
